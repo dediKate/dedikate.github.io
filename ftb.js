00afa5a4ad19f2c894d0c52489e0030ae86c2f50
dediKate <dedikate04@gmail.com>
2019-11-22 23:33:58 -0500

    Update ftb.js
    Added more options.

--- ftb.js
+++ ftb.js
@@ -8,8 +8,15 @@ var fun = [
   'You don\'t remember starting on the floor, but that\'s where you ended up.',
   'Well, someone blew it early, leaving the other completely unsatisfied.',
   'It was like two strays met in the street and went at each other. You\'re both covered in scratches, bruises and bite marks.',
-  'That round of boning was so good, you probably need a smoke, or drink, or whatever it is you do after a good time.'
-      // 'Results go here', comma after each, \' for each apostrophe //
+  'That round of boning was so good, you probably need a smoke, or drink, or whatever it is you do after a good time.',
+  'Fast, furious, full of passion. This one was the real deal. Might need to talk about this one afterwards.',
+  'In the middle of everything, someone went for the butt without asking.',
+  'You decided to get real verticle and someone got pinned to the wall. It would have been easier on the bed, but you did it for the hotness factor.',
+  'In a fit of passion, someone ended up on all fours and took it from behind.',
+  'You got frisky and went down on your partner. And they got frisky and went down on you at the same time.',
+  'You broke out the handcuffs, tied your partner down, and went at them for all their worth.',
+  'Legs? See shoulders. And then someone got fucked within an inch of their life.'
+        // 'Results go here', comma after each, \' for each apostrophe //
   ]
 
 function newFun() {
